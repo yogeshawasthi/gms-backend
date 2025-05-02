a724ca28ee3401ccf5ef053ad03dcc5dc1e63b62
Yogesh Awasthi <yogeshawasthi54321@gmail.com>
2025-05-02 13:24:54 +0545

    error msg

--- DBConn/conn.js
+++ DBConn/conn.js
@@ -5,5 +5,5 @@ mongoose.connect("mongodb://localhost:27017/gymBackend")
         console.log("Connected to MongoDB successfully & PeaceFully");
     })
     .catch((err) => {
-        console.log("Error connecting to MongoDB", err);
+        console.log("Error connecting to MongoDB oh sorry", err);
     });
