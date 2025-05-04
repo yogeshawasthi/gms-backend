da7c1da669a49cc7f6ed809f1cad19209acd3c87
Yogesh Awasthi <yogeshawasthi54321@gmail.com>
2025-05-04 19:16:23 +0545

    few changes

--- index.js
+++ index.js
@@ -6,12 +6,15 @@ const PORT = 4000;
 require('./DBConn/conn.js');
 
 const GymRoutes = require('./Routes/gym');
+const MembershipRoutes = require('./Routes/membership');
 
 // Add middleware to parse JSON request bodies
 app.use(express.json()); // This is required to parse JSON data in req.body
 
+// Define routes
 app.use('/auth', GymRoutes);
+app.use('/plans', MembershipRoutes);
 
 app.listen(PORT, () => {
-    console.log("Server is running on Port 4000");
+    console.log(`Server is running on Port ${PORT}`);
 });
