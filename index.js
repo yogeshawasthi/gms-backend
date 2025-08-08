4a160f869f34db08f458f40424d22f29701006e3
Yogesh Awasthi <yogeshawasthi54321@gmail.com>
2025-08-08 13:48:01 +0545

    .

--- index.js
+++ index.js
@@ -9,8 +9,19 @@ const connectDB = require('./DBConn/conn.js');
 
 connectDB(); // Connect to MongoDB
 
+const allowedOrigins = [
+  'http://localhost:3000',
+  'https://gym-8e43s0pu0-yogesh-awasthis-projects.vercel.app'
+];
+
 app.use(cors({
-  origin: 'https://gym-8e43s0pu0-yogesh-awasthis-projects.vercel.app', // your Vercel frontend URL
+  origin: function(origin, callback){
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  },
   credentials: true
 }));
 
